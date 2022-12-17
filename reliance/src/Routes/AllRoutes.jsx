cb5003909a25499066c24310654079ccb86687ae
Adham Kazi <kaziadham95@gmail.com>
2022-12-17 14:58:10 +0530

    add

--- reliance/src/Routes/AllRoutes.jsx
+++ reliance/src/Routes/AllRoutes.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from '../Components/Home'
-import Login from '../Components/Login'
-import Signup from '../Components/Signup'
+// import Login from '../Components/Login'
+// import Signup from '../Components/Admin/Signup'
 import UserRegister from '../Pages/UserRegister'
+import Cart from "../Pages/CartAndPayment/Cart"
+import Payment from './../Pages/CartAndPayment/Payment';
 
 const AllRoutes = () => {
   return (
     <div>
-        <Routes>
-            <Route path='/' element={<Home/>}></Route>
-            <Route path='/login' element={<Login/>}></Route>
-            <Route path='/signup' element={<Signup/>}></Route>
-            <Route path='/register' element={<UserRegister/>}></Route>
-        </Routes>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/cart' element={<Cart />}></Route>
+        <Route path="/checkout" element={<Payment />}></Route>
+        <Route path='/register' element={<UserRegister />}></Route>
+      </Routes>
     </div>
   )
 }
